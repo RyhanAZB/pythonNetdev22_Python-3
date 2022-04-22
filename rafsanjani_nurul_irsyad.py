11f62b672ce1447a974d2ba6699f3fae65ba0b02
Naufal Nurul Irsyad <45141323+maplezs@users.noreply.github.com>
2022-04-22 22:38:18 +0700

    upload new file

---
+++ rafsanjani_nurul_irsyad.py
@@ -0,0 +1,11 @@
+#rafsanjani nurul irsyad
+#x = int(input("banyak angka : "))
+#for y in x:
+#  nilai=int(input("input: "))
+x = 5
+def rata(x):
+  a = 0 
+  for i in x:
+    a = a + x
+  rata_2 = a/len(x)
+  print(rata_2)
